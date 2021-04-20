236f1f44d2e114e26b28391cacbc079d75778a4a
Rajendra Mohan Panda <rmp.iit.kgp@gmail.com>
2021-04-20 13:17:43 -0400

    Update Soil Class Map.R

--- Soil Class Map.R
+++ Soil Class Map.R
@@ -1,4 +1,3 @@
-options(shiny.maxRequestSize = 300*1024^5) 
 library(rlist)
 library(raster)
 library(ggplot2)
@@ -13,59 +12,66 @@ library(shiny)
 library(shinydashboard)
 library(leaflet)
 library(sf)
-# Load shapefile
-OK <- read_sf("S:/UGA-project/OK_SHP/oklahoma_extent.shp")
-# Load raster layers
-r1 <- list.files("S:/UGA-project/ppt/ppt2002", pattern = ".tif$", full.names = T)
+#Increase memory limit
+options(shiny.maxRequestSize = 300*1024^5) 
+
+# Load list of raster layers with maximum air temperature values
+r1 <- list.files("DataPath", pattern = ".tif$", full.names = T)
 # Stacking multiple rasters
-ras1 <- stack(r1) 
+rs1 <- stack(r1) 
 
-# To calculate annual max temperature
-PPTN <- calc(ras1, fun = mean, na.rm = T)
-writeRaster(PPTN, file.path("S:/UGA-project/Output/PPT/PPT2002"), driver = "GeoTiff", overwrite = TRUE)
+# To calculate maximum air temperature per each pixel
+TMAX <- calc(rs1, fun = mean, na.rm = T)
+#Save files
+writeRaster(TMAX, file.path("DataPath"), driver = "GeoTiff", overwrite = TRUE)
 
-# Load raster layers
-r2 <- list.files("S:/UGA-project/tmin/tmin2016", pattern = ".tif$", full.names = T)
-ras2 <- stack(r2) 
-# To calculate annual min temperature
-Tmin <- calc(ras2, fun = mean, na.rm = T)
-writeRaster(Tmin, file.path("S:/UGA-project/Output/TMIN/Tmin2016"), driver = "GeoTiff", overwrite = TRUE)
+# Load raster layers of minimum air temperature values
+r2 <- list.files("DataPath", pattern = ".tif$", full.names = T)
+rs2 <- stack(r2) 
+# To calculate minimum temperature at each pixel
+TMIN <- calc(rs2, fun = mean, na.rm = T)
+writeRaster(TMIN, file.path("DataPath"), driver = "GeoTiff", overwrite = TRUE)
 
-#To calculate annual mean temperature
-MAAT <- stack(Tmin, Tmax)
+#To calculate mean of maximum and minimum air temperature
+MAAT <- stack(TMIN, TMAX)
 MAAT <- calc(MAAT, fun = mean, na.rm = T)
-writeRaster(MAAT, file.path("S:/UGA-project/Output/MAAT/MAAT2016"), driver = "GeoTiff", overwrite = TRUE)
+writeRaster(MAAT, file.path("DataPath"), driver = "GeoTiff", overwrite = TRUE)
 
-#Temp <- list.files("S:/UGA-project/Output")
+# Add some increments to available mean air temperature
 MAST <- MAAT + 1
-#writeRaster(MAST, file.path("S:/UGA-project/Output/MAST"), driver = "GeoTiff", overwrite = TRUE)
+#writeRaster(MAST, file.path("DataPath"), driver = "GeoTiff", overwrite = TRUE)
 #To read raster
 #MAST <- raster("S:/UGA-project/Output/MAST.grd")
-
+# Create data frame for five class matrix
 reclass_df <- c(-Inf, 0, 1,
                 0, 8, 2,
                 8, 15, 3,
                 15, 22, 4,
                 22, Inf, 5)
-reclass_df
-
+# View details
+view(reclass_df)
+# Creates matrix 
 reclass_m <- matrix(reclass_df,
                     ncol = 3,
                     byrow = TRUE)
-
-# reclassify the raster using the reclass object - reclass_m
+view(reclass_m)
+# reclassify the raster using the matrix
 classified_Temp <- reclassify(MAST, reclass_m)
 
-# view reclassified data
-barplot(classified_Temp, main = "Number of pixels in each class")reclass_df <- c(8, 15, 1,
+# view pixel values for each class
+barplot(classified_Temp, main = "Number of pixels in each class")
+# Create dataframe for two class matrix 
+reclass_df2 <- c(8, 15, 1,
                 15, 22, 2
 )
-
-# assign all pixels that equal 0 to NA or no data value
-#chm_classified[chm_classified == 0] <- NA
-
-# plot reclassified data on basis of bar plot info
-plot(classified_Temp,
+# Creates matrix 
+reclass_m2 <- matrix(reclass_df2,
+                    ncol = 3,
+                    byrow = TRUE)
+# reclassify the raster using the matrix
+classified_Temp2 <- reclassify(MAST, reclass_m)
+# Reclassified data into two class map on basis of bar plot info
+plot(classified_Temp2,
      legend = FALSE,
      col = c("palegoldenrod", "palegreen2"), axes = TRUE,
      main = "Classified Soil Temperature")
@@ -76,17 +82,17 @@ legend("bottom",
        border = FALSE,
        bty = "n") # turn off legend border
 
-# Crop using extent, rasterize polygon and finally, create poly-raster
-#          **** This is the code that you are after ****  
-
-OK_STR <- mask(classified_Temp, mask=OK)
-
-# Plot results
+# Crop to shapefile extent
+# Load shapefile
+OK <- read_sf("DataPath/_extent.shp")
+OK_STR <- mask(classified_Temp2, mask=OK)
 plot(OK_STR)
-classified_OK_STR <- reclassify(OK_STR, reclass_m)
 
-# plot reclassified data
-plot(classified_OK,
+#Alternate options
+classified_OK_STR <- reclassify(OK_STR, reclass_m2)
+
+# plot customised classified maps for the masked area 
+plot(classified_OK_STR,
      legend = FALSE,
      col = c("palegoldenrod", "palegreen2"), axes = TRUE,
      main = "Classified Soil Temperature")
